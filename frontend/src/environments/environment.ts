ae858f01816dfefde2ef974f6d7c8e9716b676e4
Rio Rizki <rio.rizkiw21@gmail.com>
2020-04-08 00:19:32 +0700

    init

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,14 @@
+/* @TODO replace with your variables
+ * ensure all variables on this page match your project
+ */
+
+export const environment = {
+  production: false,
+  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  auth0: {
+    url: 'riorizki-udacity', // the auth0 domain prefix
+    audience: 'coffeshop', // the audience set for the auth0 app
+    clientId: '2BLGBP46aMj0PXTMad7U5jRtVr4CZRXM', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:4200', // the base url of the running ionic application.
+  },
+};
